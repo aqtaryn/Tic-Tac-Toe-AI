092b08b825d027e01b97c47b2bc724d356a86de7
MikeAustinn <103225984+MikeAustinn@users.noreply.github.com>
2022-06-01 12:16:27 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -24,5 +24,51 @@ def update():
     pass
 
 def win_check():
-    # Only needs to be used 5 moves in
-    pass
+    import main as m
+
+
+    winner = None
+    #horizontal winner
+    def horizontalwin(board):
+        global winner
+        if board[0] == board[1] == board[2] and board[0]!= " ":
+            winner = board[0]
+            return True
+        elif board[3] == board[4] == board[5] and board[3] != " ":
+            winner = board[3]
+            return True
+        elif board[6] == board[7] == board[8] and board[6]!= " ":
+            winner = board[6]
+            return True
+    #vertical winner
+    def verticalwin(board):
+        global winner
+        if board[0] == board[3] == board[6] and board[0]!= " ":
+            winner = board[0]
+            return True
+        elif board[1] == board[4] == board[7] and board[1] != " ":
+            winner = board[1]
+            return True
+        elif board[2] == board[5] == board[8] and board[2]!= " ":
+            winner = board[2]
+            return True
+    #diagonal winner
+    def diagonalwin(board):
+        global winner
+        if board[0] == board[4] == board[8] and board[0]!= " ":
+            winner = board[0]
+            return True
+        elif board[2] == board[4] == board[6] and board[2] != " ":
+            winner = board[2]
+            return True
+    #checks if is a cat game
+    def nowin(board):
+        if " " not in board:
+            print(m.visual_board)
+            print("There was no winner :(")
+            quit()
+
+    def display_winner(board):
+        global winner
+        if horizontalwin(board) or verticalwin(board) or diagonalwin(board):
+            print("The winner is {}".format(winner))
